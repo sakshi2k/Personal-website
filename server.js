5c223d46d15916abc5f5035005357690a1fd6c60
Sakshi2k <sakshi2k@yahoo.co.in>
2020-06-04 16:12:28 +0530

    added files

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({  extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", async(req, res) => {
+  res.render("index");
+})
+
+app.get("/projects", async(req, res) => {
+  res.render("projects");
+})
+
+app.get("/resume", async(req, res) => {
+  res.render("resume");
+})
+
+app.get("/contact", async(req, res) => {
+  res.render("contact");
+})
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
