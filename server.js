4dbd00e7a8b5a778c564853aaa464828a96d3b19
Sakshi2k <sakshi2k@yahoo.co.in>
2020-06-15 22:18:27 +0530

    bug fixed

--- server.js
+++ server.js
@@ -27,8 +27,6 @@ app.get("/contact", async(req, res) => {
   res.render("contact");
 })
 
-// const PORT;
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port");
 });
