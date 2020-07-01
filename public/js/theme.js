6769e3355cdb91b6d7bcb76a859ee46fed980146
Sakshi2k <sakshi2k@yahoo.co.in>
2020-07-01 21:07:16 +0530

    UI improved

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,21 +1,19 @@
 function retheme() {
     var element = document.body;
     var navLinks = document.getElementsByClassName("nav-link");
-    var navBar = document.getElementsByClassName("navbar");
-    var faLink = document.getElementsByClassName("fa");
-    var scrollToTop = document.querySelector(".scrollToTop");
+    var navbarIconToggler = document.getElementsByClassName("navbar-toggler")[0];
     
     element.classList.toggle("dark-mode");
 
     for (i=0; i < navLinks.length; i++) {
       navLinks[i].classList.toggle("darkModeLightProps");
+      if(getComputedStyle(navLinks[i]).color === "rgba(0, 0, 0, 0.5)") {
+        navLinks[i].style.color = "white";
+        navbarIconToggler.style.backgroundColor = "rgb(255, 255, 255,0.6)";
+      }
+      else {
+        navLinks[i].style.color = "rgba(0, 0, 0, 0.5)";
+        navbarIconToggler.style.backgroundColor = "rgb(255, 255, 255)";
+      }
     }
-
-    for (i=0; i < faLink.length; i++) {
-      faLink[i].classList.toggle("darkModeLightProps")
-    }
-
-    navBar[0].classList.toggle("navbar-dark");
-
-    scrollToTop.classList.toggle("darkModeScrollToTop");    
   }
