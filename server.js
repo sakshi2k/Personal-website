7befefe1a3ca14a901b97fecaa58e4aeb70f3b87
Sakshi2k <sakshi2k@yahoo.co.in>
2020-09-27 20:30:26 +0530

    contact form changed

--- server.js
+++ server.js
@@ -6,82 +6,84 @@ const https = require("https");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({  extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-require('dotenv').config();
+require("dotenv").config();
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
   res.render("home");
-})
+});
 
-app.get("/projects", async(req, res) => {
+app.get("/projects", async (req, res) => {
   res.render("projects");
-})
+});
 
-app.get("/resume", async(req, res) => {
+app.get("/resume", async (req, res) => {
   res.render("resume");
-})
+});
 
-app.get("/contact", async(req, res) => {
-  res.render("contact", {successMessage : ""});
-})
+app.get("/contact", async (req, res) => {
+  res.render("contact", { successMessage: "" });
+});
 
 app.post("/send", (req, res) => {
-
-  const fullName = req.body.name;
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
   const email = req.body.email;
   const subject = req.body.subject;
   const message = req.body.message;
 
-  //processing Fullname
-  var firstName = fullName.split(' ').slice(0, -1).join(' ');
-  var lastName = fullName.split(' ').slice(-1).join(' ');
-
   const messageData = {
-  members : [
+    members: [
       {
-      email_address: email,
-      status : "subscribed",
-      merge_fields : {
-          FNAME : firstName,
-          LNAME : lastName,
-          MESSAGE : message,
-          SUBJECT : subject
-        }
-      }
-  ]   
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+          MESSAGE: message,
+          SUBJECT: subject,
+        },
+      },
+    ],
   };
 
   const jsonData = JSON.stringify(messageData);
 
-  const url = "https://us8.api.mailchimp.com/3.0/lists/"+process.env.MC_ListID;
+  const url =
+    "https://us8.api.mailchimp.com/3.0/lists/" + process.env.MC_ListID;
   const options = {
-      method: "POST",
-      auth: process.env.MC_API_Key
+    method: "POST",
+    auth: process.env.MC_API_Key,
   };
 
-  const request = https.request(url, options, function(response) {
-      response.on("data", (data) => {
-          console.log(JSON.parse(data));
-      })
+  const request = https.request(url, options, function (response) {
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
 
-      if(response.statusCode ===200){
-        res.render("contact", {successMessage : "Congratulations. Your message has been successfully sent. Thanks for reaching out.!"});
-      }
-      else {
-        res.render("contact", {successMessage : "Sorry, some issue occurred. Try again?"});
-        console.log("Failed");        
-      }
-  })
+    if (response.statusCode === 200) {
+      res.render("contact", {
+        successMessage:
+          "Congratulations. Your message has been successfully sent. Thanks for reaching out.!",
+      });
+    } else {
+      res.render("contact", {
+        successMessage: "Sorry, some issue occurred. Try again?",
+      });
+      console.log("Failed");
+    }
+  });
 
   request.write(jsonData);
   request.end();
-
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  process.env.PORT ? console.log("Server started on port") : console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+  process.env.PORT
+    ? console.log("Server started on port")
+    : console.log("Server started on port 3000");
 });
