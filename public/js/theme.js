56b3a1855ccde023be3d02fff481083e22f1d652
Sakshi2k <sakshi2k@yahoo.co.in>
2020-11-06 00:47:27 +0530

    toggle switch implemented

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,28 +1,72 @@
 function retheme() {
-    var element = document.body;
-    var faIcons = document.getElementsByClassName("fa");
-    var navLinks = document.getElementsByClassName("nav-link");
-    var navbarIconToggler = document.getElementsByClassName("navbar-toggler")[0];
-    
-    element.classList.toggle("dark-mode");
+  var element = document.body;
+  var faIcons = document.getElementsByClassName("fa");
+  var navLinks = document.getElementsByClassName("nav-link");
+  var navbarIconToggler = document.getElementsByClassName("navbar-toggler")[0];
 
-    for (var i=0; i < navLinks.length; i++) {
-      navLinks[i].classList.toggle("darkModeLightProps");
-      if(getComputedStyle(navLinks[i]).color === "rgba(0, 0, 0, 0.5)") {
-        navLinks[i].style.color = "white";
-        navbarIconToggler.style.backgroundColor = "rgb(13, 140, 160)";
-      }
-      else {
-        navLinks[i].style.color = "rgba(0, 0, 0, 0.5)";
-        navbarIconToggler.style.backgroundColor = "rgb(255, 255, 255)";
-      }
+  element.classList.toggle("dark-mode");
+
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.toggle("darkModeLightProps");
+    if (getComputedStyle(navLinks[i]).color === "rgba(0, 0, 0, 0.5)") {
+      navLinks[i].style.color = "white";
+      navbarIconToggler.style.backgroundColor = "rgb(13, 140, 160)";
     }
+    else {
+      navLinks[i].style.color = "rgba(0, 0, 0, 0.5)";
+      navbarIconToggler.style.backgroundColor = "rgb(255, 255, 255)";
+    }
+  }
 
-    for (var i=11; i < faIcons.length; i++) {
-      if(faIcons[i].style.color !== "white"){
-        faIcons[i].style.color = "white";
+  for (var i = 12; i < faIcons.length; i++) {
+    if (faIcons[i].style.color !== "white") {
+      faIcons[i].style.color = "white";
+    }
+    else
+      faIcons[i].style.color = "rgb(0, 0, 0)";
+  }
+}
+
+
+// Omlet toggle
+if (location.pathname === '/') {
+  var select = function (s) {
+    return document.querySelector(s);
+  },
+    selectAll = function (s) {
+      return document.querySelectorAll(s);
+    },
+    animationWindow = select('#animationWindow'),
+    animData = {
+      wrapper: animationWindow,
+      animType: 'svg',
+      loop: false,
+      prerender: false,
+      autoplay: false,
+      path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/egg_flip.json'
+    }, anim;
+
+  anim = bodymovin.loadAnimation(animData);
+  anim.addEventListener('DOMLoaded', onDOMLoaded);
+  anim.setSpeed(15);
+
+  function onDOMLoaded(e) {
+
+    animationWindow.onclick = function (e) {
+      if (anim.currentFrame > 0) {
+        anim.playSegments([anim.currentFrame, 0], true);
+        TweenMax.to('.eggGroup', 1, {
+          x: 0,
+          ease: Elastic.easeOut.config(0.9, 0.38)
+        })
+      } else {
+        TweenMax.to('.eggGroup', 1.4, {
+          x: 73,
+          ease: Elastic.easeOut.config(0.9, 0.38)
+        })
+        anim.playSegments([anim.currentFrame, 300], true)
       }
-      else
-      faIcons[i].style.color ="rgb(0, 0, 0)";
+      retheme();
+    }
   }
 }
